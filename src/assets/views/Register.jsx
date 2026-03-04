cdec4f8d781c03837d961d3945dcf8eccbbd612a
zoevalverde <zoevalverde00@gmail.com>
2026-03-04 19:55:16 -0300

    add: view Register

---
+++ src/assets/views/Register.jsx
@@ -0,0 +1,20 @@
+const Register = () => {
+  return (
+    <section>
+      <h2>REGISTRATE</h2>
+      <form >
+        <input
+          type="email"
+          placeholder="correo electronico"
+        />
+        <input
+          type="password"
+          placeholder="contraseña"
+        />
+        <button>INGRESAR</button>
+      </form>
+    </section>
+  )
+}
+
+export {Register }
