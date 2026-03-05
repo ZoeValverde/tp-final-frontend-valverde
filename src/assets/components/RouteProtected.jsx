d331b2656bbfa0194b3c339682b43d3eb0373e5b
zoevalverde <zoevalverde00@gmail.com>
2026-03-04 22:12:18 -0300

    feat: funtion in Register and refactor login

--- src/assets/components/RouteProtected.jsx
+++ src/assets/components/RouteProtected.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { ChatContext } from "../context/ChatContext"
+import { ChatContext } from "../context/chatContext"
 import { Navigate } from "react-router-dom"
 
 const RouteProtected = ({ children }) => {
--- src/assets/views/Register.jsx
+++ src/assets/views/Register.jsx
@@ -1,20 +1,54 @@
+import { useContext, useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { ChatContext } from "../context/chatContext"
+
 const Register = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+
+  const { handleAccounts } = useContext(ChatContext)
+  const navigate = useNavigate()
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        if(!email || !password || !name){return}
+
+    
+      handleAccounts({ email, name, password })
+      
+    navigate("/")
+  }
+
   return (
     <section>
       <h2>REGISTRATE</h2>
-      <form >
+      <form onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder="correo electronico"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="nombre de usuario"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input
           type="password"
           placeholder="contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button>INGRESAR</button>
+        <button type="submit">INGRESAR</button>
       </form>
     </section>
   )
 }
 
-export {Register }
+export { Register }
